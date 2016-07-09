519e674d072a7279c750419ad60a4d1cde00f4b2
1103546434 <1103546434@qq.com>
2016-07-09 20:26:43 +0800

    7-9-sh-1

--- www/js/searceh.js
+++ www/js/searceh.js
@@ -1,4 +1,15 @@
 angular.module('starter.searceh', [])
-  .controller('searcehController', ['$scope', function ($scope) {
-  	
+  .controller('searcehController', ['$scope','$http', function ($scope,$http) {
+			$scope.searchlist='';
+			$http({
+				url:"../mock/search.json",
+				method:'get'
+				})
+			.then(function(res){
+				$scope.searchlist=res.data;
+
+			})
+			
+			
+			
   }])
